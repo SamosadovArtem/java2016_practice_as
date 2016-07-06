23dd5b563f5eec2007a517aafd2323f00f036cd0
ArtemSamosadov <pizzaeueu@gmail.com>
2016-07-06 14:10:05 +0300

    First commit. Hello world by Artem

---
+++ src/main/java/com/inquirer/servlets/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.inquirer.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Admin on 06.07.2016.
+ */
+public class HelloServlet  extends HttpServlet {
+
+    private String responseTemplate =
+            "<html>\n" +
+                    "<body>\n" +
+                    "<h2>Hello from Simple Servlet</h2>\n" +
+                    "</body>\n" +
+                    "</html>";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.process(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.process(request, response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(200);
+        response.getWriter().write(responseTemplate);
+    }
+}
+
