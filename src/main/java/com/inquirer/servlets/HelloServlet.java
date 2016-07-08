9cf1a3f416d5f48fe4e66c6ecf8c521021618a23
Yegor <inferno2596@gmail.com>
2016-07-08 13:15:02 +0300

    Add dependency in pom.xml, add listener in web.xml, add tiles.xml, add 3 jsp pages, add style.css by Yegor

--- src/main/java/com/inquirer/servlets/HelloServlet.java
+++ src/main/java/com/inquirer/servlets/HelloServlet.java
@@ -1,5 +1,11 @@
 package com.inquirer.servlets;
 
+import org.apache.tiles.TilesContainer;
+import org.apache.tiles.access.TilesAccess;
+import org.apache.tiles.request.Request;
+import org.apache.tiles.request.servlet.ServletApplicationContext;
+import org.apache.tiles.request.servlet.ServletRequest;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,26 +16,28 @@ import java.io.IOException;
  */
 public class HelloServlet  extends HttpServlet {
 
-    private String responseTemplate =
-            "<html>\n" +
-                    "<body>\n" +
-                    "<h2>Hello from Simple Servlet</h2>\n" +
-                    "</body>\n" +
-                    "</html>";
+//    private String responseTemplate =
+//            "<html>\n" +
+//                    "<body>\n" +
+//                    "<h2>Hello from Simple Servlet</h2>\n" +
+//                    "</body>\n" +
+//                    "</html>";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        this.process(request, response);
+        TilesContainer container = TilesAccess.getContainer(new ServletApplicationContext(request.getSession().getServletContext()));
+        Request req = new ServletRequest(container.getApplicationContext(), request, response);
+        container.render("inquirer.homepage", req);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        this.process(request, response);
-    }
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        this.process(request, response);
+//    }
 
-    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setStatus(200);
-        response.getWriter().write(responseTemplate);
-    }
+//    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setStatus(200);
+//        response.getWriter().write(responseTemplate);
+//    }
 }
 
---
+++ src/main/webapp/WEB-INF/jsp/results_page.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inquirer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <h1>
+            Опрос завершен!
+        </h1>
+    </div>
+    <div class="result_block">
+        <h2>Результат: 10 баллов</h2>
+    </div>
+    <div class="button_links_block">
+        <form action="">
+            <input class="link" class="result_page_button" type="button" name="" value="Еще раз" onclick="location.href='./inquirer.html'">
+            <input class="link" class="result_page_button" type="button" name="" value="Выйти" onclick="location.href='./login_page.html'">
+        </form>
+    </div>
+</div>
+</body>
+</html>
