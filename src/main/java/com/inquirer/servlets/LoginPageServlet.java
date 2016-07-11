2f37864f0a1a46eb479444a99d4e906c77904894
Yegor <inferno2596@gmail.com>
2016-07-11 12:54:53 +0300

    Add 2 more servlets for last jsp pages by Yegor

---
+++ src/main/java/com/inquirer/servlets/LoginPageServlet.java
@@ -0,0 +1,25 @@
+package com.inquirer.servlets;
+
+import org.apache.tiles.TilesContainer;
+import org.apache.tiles.access.TilesAccess;
+import org.apache.tiles.request.Request;
+import org.apache.tiles.request.servlet.ServletApplicationContext;
+import org.apache.tiles.request.servlet.ServletRequest;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Егор on 11.07.2016.
+ */
+public class LoginPageServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        TilesContainer container = TilesAccess.getContainer(new ServletApplicationContext(request.getSession().getServletContext()));
+        Request req = new ServletRequest(container.getApplicationContext(), request, response);
+        container.render("inquirer.loginPage", req);
+    }
+}
